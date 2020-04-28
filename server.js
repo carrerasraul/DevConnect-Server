389a1e43d1b3da78d10256f35a2e5efd792030bf
Raul Carreras <raul.e.carreras@gmail.com>
2020-04-27 20:25:28 -0400

    CORS update

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ const app = express();
 
 app.use(cors());
 
-// CORS HEADERS
-app.use((req, res, next) => {
-  // Controls which domains should have access
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Controls which headers incoming requests may have, so they are handled
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  // Controls which HTTP methods may be used on the front end
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-
-  next();
-});
-
 // Connect Database
 connectDB();
 
