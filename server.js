16a5b613a4d33387b2387106e8dfc5c658bd57a3
Raul Carreras <raul.e.carreras@gmail.com>
2020-05-11 02:37:07 -0400

    test

--- server.js
+++ server.js
@@ -25,3 +25,5 @@ app.use('/api/posts', require('./routes/api/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
